1dd2eb9d43552866a7504516a6e8c9e127b0b6bb
jesseTitus <jessejtitus@gmail.com>
2025-07-08 17:07:54 -0400

    feat: add TestSecurityConfig to resolve OAuth2 bean dependencies in tests
    - Create TestSecurityConfig class with @TestConfiguration annotation
    - Provide dummy ClientRegistrationRepository bean for test context
    - Configure mock Auth0 client registration with required OAuth2 properties
    - Resolve context loading errors in repository tests
    - Enable tests to run without actual Auth0 environment variables

---
+++ src/test/java/com/titus/developer/jugtours/TestSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.titus.developer.jugtours;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
+
+@TestConfiguration
+public class TestSecurityConfig {
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        // Create a dummy client registration for testing
+        ClientRegistration clientRegistration = ClientRegistration.withRegistrationId("auth0")
+                .clientId("test-client-id")
+                .clientSecret("test-client-secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
+                .scope("openid", "profile", "email")
+                .authorizationUri("https://test.auth0.com/authorize")
+                .tokenUri("https://test.auth0.com/oauth/token")
+                .userInfoUri("https://test.auth0.com/userinfo")
+                .userNameAttributeName(IdTokenClaimNames.SUB)
+                .jwkSetUri("https://test.auth0.com/.well-known/jwks.json")
+                .clientName("Auth0")
+                .build();
+
+        return new InMemoryClientRegistrationRepository(clientRegistration);
+    }
+} 
