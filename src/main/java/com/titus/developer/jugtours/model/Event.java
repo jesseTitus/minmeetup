8de56c5f4f1368874b255a03b39a73ee2edae795
jesseTitus <jessejtitus@gmail.com>
2025-08-11 23:40:32 -0400

    fix: add missing GenerationType import
    Add missing jakarta.persistence.GenerationType import to resolve compilation error.

--- src/main/java/com/titus/developer/jugtours/model/Event.java
+++ src/main/java/com/titus/developer/jugtours/model/Event.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
